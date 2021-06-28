4fbb2313ad1c5689c4d76273d9051483c6808451
zhangyajun <1339811979@qq.com>
2021-06-28 21:37:04 +0800

    fix

--- future-file/src/main/java/top/goodz/future/service/utils/ImageVerificationUtil.java
+++ future-file/src/main/java/top/goodz/future/service/utils/ImageVerificationUtil.java
@@ -37,9 +37,9 @@ public class ImageVerificationUtil {
     private static final Logger LOGGER = LoggerFactory.getLogger(ImageVerificationUtil.class);
 
 
-    private static final int DEFAULT_IMAGE_WIDTH = 280;
+    private static final int DEFAULT_IMAGE_WIDTH = 410;
 
-    private static final int DEFAULT_IMAGE_HEIGHT = 171;
+    private static final int DEFAULT_IMAGE_HEIGHT = 200;
 
 
     /**

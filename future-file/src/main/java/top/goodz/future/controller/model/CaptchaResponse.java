72804f2ef295fd498b871a0854221706a244de55
zhangyajun <1339811979@qq.com>
2021-06-30 23:47:10 +0800

    图片验证码 3

---
+++ future-file/src/main/java/top/goodz/future/controller/model/CaptchaResponse.java
@@ -0,0 +1,15 @@
+package top.goodz.future.controller.model;
+
+import lombok.Data;
+
+/**
+ *  @Description TODO
+ *  @Author Yajun.Zhang
+ *  @Date 2021/6/30 22:56
+ */
+
+@Data
+public class CaptchaResponse {
+
+    private String captchaImage;
+}

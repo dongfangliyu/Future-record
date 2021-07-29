ff8907313dcb433efdff56518ab27699f3dba4a9
zhangyajun <1339811979@qq.com>
2021-07-30 01:01:18 +0800

    数据中台模块初始化

---
+++ future-dcs/src/main/java/top/goodz/future/FutureDcsApplication.java
@@ -0,0 +1,13 @@
+package top.goodz.future;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutureDcsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FutureDcsApplication.class, args);
+    }
+
+}
